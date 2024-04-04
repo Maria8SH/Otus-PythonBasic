93966d17b59c3e17bd7bbe56887df4ea1385ddb7
Maria8SH <mshlyakova18@gmail.com>
2024-04-04 18:34:47 +0300

    add homework02

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,25 @@
 from abc import ABC
 
+from homework_02.exceptions import NotEnoughFuel, LowFuelError
+
 
 class Vehicle(ABC):
-    pass
+    def __init__(self, weight=0, fuel=0, fuel_consumption=1):
+        self.weight = weight
+        self.started = False
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    def start(self):
+        if not self.started:
+            if self.fuel > 0:
+                self.started = True
+            else:
+                raise LowFuelError("Low Fuel")
+
+    def move(self, distance):
+        required_fuel = distance * self.fuel_consumption
+        if required_fuel <= self.fuel:
+            self.fuel -= required_fuel
+        else:
+            raise NotEnoughFuel("Not Enough Fuel")
--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,25 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+
+
+from homework_02.base import Vehicle
+from homework_02.exceptions import CargoOverload
+
+
+class Plane(Vehicle):
+    def __init__(self, weight=0, started=False, fuel=0, max_cargo=100):
+        super().__init__(weight, started, fuel)
+        self.cargo = 0
+        self.max_cargo = max_cargo
+
+    def load_cargo(self, amount):
+        if self.cargo + amount <= self.max_cargo:
+            self.cargo += amount
+        else:
+            raise CargoOverload("Cargo Overload")
+
+    def remove_all_cargo(self):
+        previous_cargo = self.cargo
+        self.cargo = 0
+        return previous_cargo
