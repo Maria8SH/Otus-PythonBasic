87029cf8a6650516a2496292cd72f57e04cf7587
Maria8SH <mshlyakova18@gmail.com>
2024-08-21 15:59:50 +0300

    working

--- homework_04/models.py
+++ homework_04/models.py
@@ -10,7 +10,48 @@
 
 import os
 
+from typing import TYPE_CHECKING, Text
+from datetime import datetime
+
+from sqlalchemy import create_engine
+from sqlalchemy import DateTime
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import declared_attr
+from sqlalchemy.orm import relationship
+
+
 PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
 
 Base = None
 Session = None
+
+engine = create_engine(url=PG_CONN_URI)
+
+
+class Base(DeclarativeBase):
+
+    @declared_attr.directive
+    def __tablename__(cls):
+        return f"{cls.__name__.lower()}s"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+
+class User(Base):
+    name: Mapped[str] = mapped_column(String(32), unique=True)
+    username: Mapped[str] = mapped_column(String(32), unique=True)
+    email: Mapped[str | None] = mapped_column(unique=True)
+    posts: Mapped[list["Post"]] = relationship(back_populates="author")
+
+
+class Post(Base):
+    title: Mapped[str] = mapped_column(String(100))
+    body: Mapped[str] = mapped_column(Text)
+    published_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=False))
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    author: Mapped["User"] = relationship(back_populates="posts")
+
